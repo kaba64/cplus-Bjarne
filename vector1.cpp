3d62dfc8d29a93624df7a1e570ec3aaf0d84dfb4
Kazem Bazesefidpar <bazesefidpar64@gmail.com>
2022-07-23 15:45:57 +0200

    My initial commit
    Signed-off-by: Kazem Bazesefidpar <bazesefidpar64@gmail.com>

--- vector1.cpp
+++ vector1.cpp
@@ -16,8 +16,8 @@ auto main() -> int
   int newd = ptr;
   std::cout<<ptr<<std::endl;
   std::vector<double> age(4);
-  //age[0]=0.33;
-  //age[1]=22.0;
-  //age[2]=27.2;
-  //age[3]=54.2;
+  age[0]=0.33;
+  age[1]=22.0;
+  age[2]=27.2;
+  age[3]=54.2;
 }
