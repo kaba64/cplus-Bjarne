9e420552b5c8e4f6122ec4990f9630a464e8682c
Kazem Bazesefidpar <bazesefidpar64@gmail.com>
2023-10-10 10:36:08 +0200

    Example of the book!

---
+++ ch3/3-1-Input/input.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+auto main() -> int
+{
+  std::cout<<"Please enter your first name:\n"<<std::endl;
+  std::string first_name;
+  std::cin>>first_name;
+  std::cout<<"Hello, "<<first_name<<std::endl;
+}
