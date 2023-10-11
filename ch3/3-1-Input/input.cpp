d4da0d2a99054d43d6db8d95426f2eb918558b0e
Kazem Bazesefidpar <bazesefidpar64@gmail.com>
2023-10-11 10:45:29 +0200

    Cleaned

--- ch3/3-1-Input/input.cpp
+++ ch3/3-1-Input/input.cpp
@@ -2,7 +2,7 @@
 
 auto main() -> int
 {
-  std::cout<<"Please enter your first name:\n"<<std::endl;
+  std::cout<<"Please enter your first name:"<<std::endl;
   std::string first_name;
   std::cin>>first_name;
   std::cout<<"Hello, "<<first_name<<std::endl;
