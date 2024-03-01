01fc5402de65fbe741c97b4730aa2576fc977b3b
kaba64 <Kazem Bazesefidpar>
2024-03-01 10:21:35 +0100

    Adding exception handling!

--- ch10/10-4-OF/open-file.cpp
+++ ch10/10-4-OF/open-file.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <exception>
+#include <string>
 
 class Point{
 public:
@@ -9,20 +11,28 @@ public:
   double x,y;
 };
 
-auto main() -> int
-{
+int main(int argc,char **argv){
+  
   std::vector<Point> points;
   std::cout<<"Please enter a name for the output file"<<"\n";
   std::string oname;
   std::cin>>oname;
-  std::ofstream ost{oname+".txt"};
-  if(!ost) std::cout<<"Hi"<<"\n";
-
-  std::cout<<"Please enter the points x and y"<<"\n";
-  for(double x,y;std::cin>>x>>y;){
-    Point p{x,y};
-    points.push_back(p);
+  try{
+    std::ofstream ost{oname+".txt"};
+    if(!ost) {
+      std::string error_m{"Error in opention "+oname+" file."};
+      throw std::runtime_error(error_m);
+    }
+    
+    std::cout<<"Please enter the points x and y"<<"\n";
+    for(double x,y;std::cin>>x>>y;){
+      Point p{x,y};
+      points.emplace_back(p);
+    }
+    for(unsigned int i=0;i<points.size();++i)
+      ost<<points[i].x<<"\t"<<points[i].y<<"\n";
+  }catch(const std::exception& e){
+    std::cerr << "Exception: " << e.what() <<"\n";
   }
-  for(unsigned int i=0;i<points.size();++i)
-    ost<<points[i].x<<"\t"<<points[i].y<<"\n";
+  return 0;
 }
